3a4c67ac743061203370e4f5e186f0c5e3dfda96
Yair <yair.janbar@gmail.com>
2024-11-16 19:00:59 +0200

    third q check

--- Cheers.java
+++ Cheers.java
@@ -2,5 +2,20 @@
 public class Cheers {
         public static void main(String[] args) {
 	    // Replace this comment with your code
+            String name = args[0];
+            int l = name.length();
+            int num = Integer.parseInt(args[1]);
+            for (int i=0; i<l;i++) {
+                char tav = name.charAt(i);
+                if (tav == 'A'|| tav == 'E'|| tav == 'F'|| tav == 'H'|| tav == 'I'|| tav == 'L'||
+                tav == 'M'|| tav == 'N'|| tav == 'O'|| tav == 'R'||tav == 'S'||tav == 'X'){
+                        System.out.println("Give me an " + tav + ": " + tav + "!" );
+                }
+                else System.out.println("Give me a " + tav + ": " + tav + "!" );
+            }
+            for (int n=0;n<num;n++)
+            {
+                System.out.println(name + "!!!");
+            }
         }
 }
---
+++ Math.java
@@ -0,0 +1,9 @@
+
+public class Math {
+
+    public static int random() {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'random'");
+    }
+
+}
