a42da5d50af4aae394686653a52f6730a969575f
Yair <yair.janbar@gmail.com>
2024-11-18 01:54:34 +0200

    q5 update

--- Collatz.java
+++ Collatz.java
@@ -2,5 +2,31 @@
 public class Collatz {
 	public static void main(String args[]) {
 	    // Replace this comment with your code
+		int number2 = Integer.parseInt(args[0]);
+		String s = args[1];
+		int number3=1;
+		if (s=="v") {
+			while(number3<=number2) {
+				int counter=1;
+				int number=number3;
+				System.out.print(number + " ");
+				while (number!=0)
+				{
+					if (number%2==0) number=number/2;
+					else number = (number*3)+1;
+					System.out.print(number + " ");
+					if (number==1) number=0;
+					counter++;
+				}
+				System.out.print("(" + counter + ")");
+				System.out.println("");
+				number3++;
+			}
+			System.out.println("Every one of the first " +number2+" hailstone sequences reached 1.");
+	
+		}
+		if (s=="c") {
+			System.out.println("Every one of the first " +number2+" hailstone sequences reached 1.");
+		}
 	}
 }
