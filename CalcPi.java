c8e1d7c9a14fb32f788219c1b117d0c58635f9c4
Yair <yair.janbar@gmail.com>
2024-11-18 00:49:49 +0200

    fix q3

--- CalcPi.java
+++ CalcPi.java
@@ -3,22 +3,16 @@ public class CalcPi {
 	public static void main(String [] args) { 
 	    // Replace this comment with your code
 		int num = Integer.parseInt(args[0]);
-		if (num==0) {
-			System.out.println("pi according to Java: 3.141592653589793"+"");
-			System.out.println("pi approximated:     " + 0);
-		}
-		else
-		{
-			double sum = 1.0;
-			double n;
-			for (n=1;n<num;n=n+2); { 
-				if ((n%2)==0) {
-					sum = sum + (1.0/n) ;
-				}
-				else sum = sum - (1.0/n);
-			}
-			System.out.println("pi according to Java: 3.141592653589793"+"");
-			System.out.println("pi approximated:     " + sum*4);
+		double approximatedPi = 0.0;
+        for (int i = 0; i < num; i++) {
+            if (i % 2 == 0) {
+                approximatedPi += 1.0 / (2 * i + 1);
+            } else {
+                approximatedPi -= 1.0 / (2 * i + 1);
+            }
+        }
+		approximatedPi = approximatedPi*4;	
+		System.out.println("pi according to Java: 3.141592653589793"+"");
+		System.out.println("pi approximated:     " + approximatedPi);
 		}
 	}
-}
